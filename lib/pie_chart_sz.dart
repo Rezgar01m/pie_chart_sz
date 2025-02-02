c5c7ebc2af14d1660ea32a5decd744b9d3d29352
Rezgar01m <rezgar.sasr@gmail.com>
2025-02-02 18:08:12 +0330

    added custom value list

--- lib/pie_chart_sz.dart
+++ lib/pie_chart_sz.dart
@@ -4,7 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:pie_chart_sz/SpacedDonutChar.dart';
 
 class PieChartSz extends StatelessWidget {
-  const PieChartSz({super.key});
+  List<Color>? colors = [
+    Colors.purple,
+    Colors.blue,
+    Colors.orange,
+    Colors.red,
+    Colors.teal,
+  ];
+
+  List<double>? values = [
+    50,
+    20,
+    20,
+    5,
+    5,
+  ];
+   PieChartSz({super.key,this.values,this.colors});
 
   @override
   Widget build(BuildContext context) {
