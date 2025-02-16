fddc484e29d1e1bb468d9836009507443e2c4369
Rezgar01m <rezgar.sasr@gmail.com>
2025-02-16 11:33:07 +0330

    chor:format code

--- lib/SpacedDonutChar.dart
+++ lib/SpacedDonutChar.dart
@@ -10,13 +10,18 @@ class SpacedDonutChartPainter extends CustomPainter {
   TextStyle? centerTextStyle;
   Valuesettings valueSettings;
   double gapSize = 0.2;
-  SpacedDonutChartPainter({required this.colors, required this.values,this.centerText,required this.valueSettings,required this.gapSize,required this.centerTextStyle,});
 
-
+  SpacedDonutChartPainter({
+    required this.colors,
+    required this.values,
+    this.centerText,
+    required this.valueSettings,
+    required this.gapSize,
+    required this.centerTextStyle,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
-
     final Paint paint = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = 24
@@ -39,44 +44,42 @@ class SpacedDonutChartPainter extends CustomPainter {
       // Calculate midpoint for label placement
       double midAngle = startAngle + sweepAngle / 2;
 
+      if (valueSettings.showValues) {
+        Offset lineStart = Offset(
+          size.width / 2 + cos(midAngle) * (size.width / 2.5),
+          size.height / 2 + sin(midAngle) * (size.height / 2.5),
+        );
+        Offset lineEnd = Offset(
+          size.width / 2 + cos(midAngle) * (size.width / 2),
+          size.height / 2 + sin(midAngle) * (size.height / 2),
+        );
+        Offset labelPosition = Offset(
+          size.width / 2 + cos(midAngle) * (size.width / 1.8),
+          size.height / 2 + sin(midAngle) * (size.height / 1.8),
+        );
 
-      if(valueSettings.showValues){
-      Offset lineStart = Offset(
-        size.width / 2 + cos(midAngle) * (size.width / 2.5),
-        size.height / 2 + sin(midAngle) * (size.height / 2.5),
-      );
-      Offset lineEnd = Offset(
-        size.width / 2 + cos(midAngle) * (size.width / 2),
-        size.height / 2 + sin(midAngle) * (size.height / 2),
-      );
-      Offset labelPosition = Offset(
-        size.width / 2 + cos(midAngle) * (size.width / 1.8),
-        size.height / 2 + sin(midAngle) * (size.height / 1.8),
-      );
+        // Draw connecting line outside the chart
+        final Paint linePaint = Paint()
+          ..color = colors[i]
+          ..strokeWidth = 2;
+        canvas.drawLine(lineStart, lineEnd, linePaint);
 
-      // Draw connecting line outside the chart
-      final Paint linePaint = Paint()
-        ..color = colors[i]
-        ..strokeWidth = 2;
-      canvas.drawLine(lineStart, lineEnd, linePaint);
-
-      // Draw value text outside the chart
-      final textPainter = TextPainter(
-        text: TextSpan(
-          text: values[i].toString(),
-          style: valueSettings.ValueTextStyle,
-        ),
-        textDirection: TextDirection.ltr,
-      );
-      textPainter.layout(minWidth: 0, maxWidth: size.width);
-      textPainter.paint(
-          canvas,
-          Offset(
-            labelPosition.dx - textPainter.width / 2,
-            labelPosition.dy - textPainter.height / 2,
-          ));
-    }
-
+        // Draw value text outside the chart
+        final textPainter = TextPainter(
+          text: TextSpan(
+            text: values[i].toString(),
+            style: valueSettings.ValueTextStyle,
+          ),
+          textDirection: TextDirection.ltr,
+        );
+        textPainter.layout(minWidth: 0, maxWidth: size.width);
+        textPainter.paint(
+            canvas,
+            Offset(
+              labelPosition.dx - textPainter.width / 2,
+              labelPosition.dy - textPainter.height / 2,
+            ));
+      }
 
       startAngle += sweepAngle + (2 * pi * gapSize / values.length);
     }
